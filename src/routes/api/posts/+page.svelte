a62cfa8e947d7bbce617cf00681718a873937ba6
sabbirz <sabbir.kuasha@gmail.com>
2023-01-13 14:34:13 +0600

    fetch client end
    fetch data from client end
    show loading status while loading data
    show error message if any

---
+++ src/routes/api/posts/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	async function getPosts() {
+		const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+		const posts = await response.json();
+		// console.log(posts);
+		return posts;
+	}
+	// getPosts();
+</script>
+
+<main>
+	<h1>Posts</h1>
+	{#await getPosts()}
+		<p>Loading...</p>
+	{:then posts}
+		<pre class="bg-slate-800">
+        {JSON.stringify(posts, null, 2)}
+    </pre>
+	{:catch error}
+		<p class="w-1/2">{error.message}</p>
+	{/await}
+</main>
