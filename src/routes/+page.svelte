db89ba7bfdb19307581ff7842ed6d3bf9e08d1bd
sabbirz <sabbir.kuasha@gmail.com>
2023-01-13 14:08:13 +0600

    self api endpoint

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	async function subscribe(event: Event) {
+		const form = event.target as HTMLFormElement;
+		const data = new FormData(form);
+
+		await fetch('/api/newsletter', {
+			method: 'POST',
+			body: data
+		});
+	}
+</script>
+
+<h1>Newsletter</h1>
+
+<form on:submit|preventDefault={subscribe}>
+	<input type="email" name="email" class="input input-primary" />
+	<button class="btn btn-primary"> Subscribe </button>
+</form>
--- src/routes/api/newsletter/+server.ts
+++ src/routes/api/newsletter/+server.ts
@@ -12,3 +12,15 @@ export const GET: RequestHandler = async (event) => {
 };
 
 // post
+export const POST: RequestHandler = async (event) => {
+	const data = await event.request.formData();
+	const email = data.get('email');
+
+	console.log(email);
+
+	return new Response(JSON.stringify({ success: true }), {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+};
